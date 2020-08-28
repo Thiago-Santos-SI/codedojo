84f455eb9ffc8c582414386ee73fa4d22cdaabc1
Thiago-Santos-SI <thiiagosantos300@gmail.com>
2020-08-28 02:41:18 -0300

    created folder structures and configured the route system

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import Routes from "./routes";
+
 import NormalLoginForm from './Components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <NormalLoginForm />
-    </div>
+    <Routes/>
   );
 }
 
---
+++ front-end/src/routes/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import Main from "../Pages/Main";
+import SignUp from "../Pages/Auth/SignUp";
+import SignIn from "../Pages/Auth/SignIn";
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Main} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+        </Switch>
+    </BrowserRouter>
+)
+
+export default Routes
